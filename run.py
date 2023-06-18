9d8701bbcb47f2616be8a2c71e97fee4d349f7fb
Matt Robenolt <matt@ydekproductions.com>
2023-06-17 18:03:01 -0700

    move the client code into where codegen will put it

--- run.py
+++ run.py
@@ -3,34 +3,9 @@ import sys
 sys.path.insert(0, ".")
 
 import connect
-from connectgen import eliza_pb2
+from elizav1 import eliza_connect, eliza_pb2
 
-ElizaServiceName = "buf.connect.demo.eliza.v1.ElizaService"
-
-
-class ElizaServiceClient:
-    def __init__(self, base_url, *, pool=None, compressor=None):
-        self._say = connect.Client(
-            pool=pool,
-            url=f"{base_url}/{ElizaServiceName}/Say",
-            response_type=eliza_pb2.SayResponse,
-            compressor=compressor,
-        )
-        self._introduce = connect.Client(
-            pool=pool,
-            url=f"{base_url}/{ElizaServiceName}/Introduce",
-            response_type=eliza_pb2.IntroduceResponse,
-            compressor=compressor,
-        )
-
-    def Say(self, req):
-        return self._say.call_unary(req)
-
-    def Introduce(self, req):
-        return self._introduce.call_server_stream(req)
-
-
-eliza_client = ElizaServiceClient(
+eliza_client = eliza_connect.ElizaServiceClient(
     "https://demo.connect.build",
     compressor=connect.GzipCompressor,
 )
